f42333d8381bfc4b39a64c9de6ae31ebd5c0b39b
Santosh Singh <ausantoshsingh@gmail.com>
2019-10-17 12:43:47 +0530

    Added entries AddPost Method

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -44,6 +44,12 @@ namespace Treehouse.FitnessFrog.Controllers
             return View();
         }
 
+        [ActionName("Add"),HttpPost]
+        public ActionResult AddPost()
+        {   
+            return View();
+        }
+
         public ActionResult Edit(int? id)
         {
             if (id == null)
